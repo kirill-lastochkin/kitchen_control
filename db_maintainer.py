0bd7fec72c2ce0d4d4c63e31eb15edfa5a10e2ff
Kirill L <platekiller@mail.ru>
2021-04-17 15:03:36 +0700

    infinished db update

--- db_maintainer.py
+++ db_maintainer.py
@@ -11,14 +11,36 @@ class DbMaintainer:
     def create_tables(self):
         with sqlite3.connect(self.name) as conn:
             cursor = conn.cursor()
-            cursor.execute("CREATE TABLE categories (id integer, category text, user integer)")
-            cursor.execute("CREATE TABLE tags (id integer, tag text, user integer)")
+            cursor.execute("CREATE TABLE categories (id integer PRIMARY KEY AUTOINCREMENT, category text, user integer)")
+            cursor.execute("CREATE TABLE tags (id integer PRIMARY KEY AUTOINCREMENT, tag text, user integer)")
             cursor.execute("""CREATE TABLE recipes
-                              (id integer, title text, category integer, ingridients text,
+                              (id integer PRIMARY KEY AUTOINCREMENT, title text, category integer, ingridients text,
                               cooking_time text, instruction text, portions text, url text,
                               pictures text, user integer)""")
-            cursor.execute("CREATE TABLE recipe_to_tags (recipe integer, tag integer)")
+            cursor.execute("CREATE TABLE recipe_to_tags (recipe integer PRIMARY KEY, tag integer)")
             conn.commit()
 
     def update_db(self, data, user_id):
-        pass
+        self.clean_db_for_user(user_id)
+
+        categories = data["categories"]
+        tags = data["tags"]
+        recipes = data["recipes"]
+
+        with sqlite3.connect(self.name) as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO categories (category, user) VALUES (?, ?)", (categories))
+
+
+    def clean_db_for_user(self, user_id):
+        with sqlite3.connect(self.name) as conn:
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT id FROM tags WHERE user = :id", {"id": user_id})
+            tags_to_remove = cursor.fetchall()
+
+            cursor.executemany("DELETE FROM recipe_to_tags WHERE tag = ?", tags_to_remove)
+            cursor.execute("DELETE FROM categories WHERE user = :id", {"id": user_id})
+            cursor.execute("DELETE FROM tags WHERE user = :id", {"id": user_id})
+            cursor.execute("DELETE FROM recipes WHERE user = :id", {"id": user_id})
+            cursor.commit()
